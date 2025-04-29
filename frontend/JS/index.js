28e8b3736ffea9464fde83354036693524f96f99
AnasEttassouli <ettassouli.anas@etu.uae.ac.ma>
2025-04-29 18:49:02 +0200

    supersion folder frontend

--- frontend/JS/index.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.getElementById("toggleMenu");
-const navList = document.querySelector("nav ul");
-
-menuIcon.addEventListener("click", () => {
-    navList.classList.toggle("show");
-});
